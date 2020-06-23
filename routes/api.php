055f8644cd2864b008d91bc71b7f3f4c177cdfa4
Andrey Suhenko <andreysuha2@gmail.com>
2020-06-23 11:56:56 +0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([ "middleware" => "guest:api", "prefix" => "auth", "namespace" => "Auth" ], function () {
+    Route::put("", "AuthenticateController@login");
+});
+Route::group([ "middleware" => "auth:api", "prefix" => "auth", "namespace" => "Auth" ], function () {
+   Route::put("logout", "AuthenticateController@logout");
+   Route::get("check", function () { return response()->json("OK", 200); });
 });
