13be47b834eb6724051ec1d041dd6993401c3652
Andrey Suhenko <andreysuha2@gmail.com>
2020-07-09 15:54:15 +0300

    add resources

---
+++ app/Http/Resources/Defect.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Defect extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public static $wrap = "defect";
+
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "priceReduction" => $this->price_reduction
+        ];
+    }
+}
