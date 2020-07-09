3698f45adb451cd4f15800db55a00ca155ef8d41
Andrey Suhenko <andreysuha2@gmail.com>
2020-07-09 16:02:35 +0300

    make store/update validation

---
+++ app/Http/Controllers/DefectsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Defect;
+use Illuminate\Http\Request;
+use App\Http\Resources\Defect as DefectResource;
+
+class DefectsController extends Controller
+{
+
+}
