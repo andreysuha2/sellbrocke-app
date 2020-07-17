73b9cabbd8e02a7811b6d62d27968076940fe466
Andrey Suhenko <andreysuha2@gmail.com>
2020-07-17 11:27:19 +0300

    trying add defects relation

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -23,4 +23,20 @@ class Device extends Model
     public function categories() {
         return $this->belongsToMany("App\Models\Device", "category_device", "device_id", "category_id");
     }
+
+    public function defects() {
+        return Defect::join("category_defect", "defect.id", "=", "category_defect.defect_id")
+                ->join("categories", "category_defect.category_id", "=", "categories.id")
+                ->join("category_device", "categories.id", "=", "category_device.id")
+                ->join("devices", "category_device.device_id", "=", "devices.id")
+                ->where("devices.id", $this->id);
+    }
+
+    public function getDefectsAttribute() {
+        if(!$this->relationLoaded('categories') || !$this->categories->first()->relationLoaded("defects")) {
+            $this->load('categories.defects');
+        }
+
+        return collect($this->categories->lists("defects"))->collapse()->unique();
+    }
 }
