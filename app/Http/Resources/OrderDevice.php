0ae2032a806cd5d53403138db756632fc189cd5f
Andrey Suhenko <andreysuha2@gmail.com>
2020-08-11 14:03:39 +0300

    make resource

--- app/Http/Resources/OrderDevice.php
+++ app/Http/Resources/OrderDevice.php
@@ -15,12 +15,35 @@ class OrderDevice extends JsonResource
     public function toArray($request)
     {
         $productsGrids = $this->device->use_products_grids ? new ProductGridCollection($this->products_grids) : null;
+        $name = $this->device->name;
+        if($productsGrids) {
+            $size = $productsGrids->firstWhere("type", "size");
+            $carrier = $productsGrids->firstWhere("type", "carrier");
+            $name = $name . " " . $size->name . " (" . $carrier->name . ")";
+        }
+        $thumbnailRecord = $this->device->attachment("thumbnail");
+        $thumbnailPath = $thumbnailRecord ? $thumbnailRecord->url : null;
 
         return [
-            "id" => $this->id,
+            "id" => $this->device->id,
+            "name" => $name,
+            "thumbnail" => $thumbnailPath,
+            "description" => $this->device->description,
+            "prices" => [
+                "base" => $this->device->base_price,
+                "discounted" => $this->getDiscounted()
+            ],
             "condition" => $this->condition,
             "defects" => new DefectsCollection($this->defects),
             "productsGrids" => $this->when($this->device->use_products_grids, $productsGrids)
         ];
+    }
+
+    private function getDiscounted() {
+        $defectsPriceReduction = $this->defects->sum("price_reduction");
+        $discountedPercent = $this->device->company->price_reduction + $defectsPriceReduction + $this->condition->discount_percent;
+        $ratio = (100 - $discountedPercent) / 100;
+        $price = round($this->device->base_price * $ratio, 2);
+        return $price > 0 ? $price : 0;
     }
 }
