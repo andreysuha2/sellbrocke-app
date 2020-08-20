55b879cb22dd56a1c040368b5a369d6791844815
Andrey Suhenko <andreysuha2@gmail.com>
2020-08-20 11:40:14 +0300

    add defect and products grids validation

--- app/Http/Requests/Order/CreateOrder.php
+++ app/Http/Requests/Order/CreateOrder.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Order;
 
+use App\Models\Device;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 
 class CreateOrder extends FormRequest
 {
@@ -23,13 +25,37 @@ class CreateOrder extends FormRequest
      */
     public function rules()
     {
-        // TODO: check use products grids, defect can be attached to device
         return [
             "devices" => "array|required",
             "devices.*.id" => "required|numeric|exists:devices,id",
             "devices.*.defects" => "array",
             "devices.*.defects.*" => "exists:defects,id",
-            "devices.*.condition" => "required|numeric|exists:conditions,id"
+            "devices.*.condition" => "required|numeric|exists:conditions,id",
+            "devices.*" => [ function($attribute, $value, $fail) {
+                $device = Device::find($value["id"]);
+                if($device) {
+                    $this->checkProductsGrid($device, $value, $fail, $attribute);
+                    $this->checkDefects($device, $value, $fail, $attribute);
+                }
+            } ]
         ];
+    }
+
+    private function checkProductsGrid($device, $input, $fail, $attribute) {
+        if($device->use_products_grids) {
+            if(!isset($input["productsGrids"])) $fail("$attribute device is used products grids");
+            else {
+                $productsGrids = $device->productsGrids()->whereIn("products_grids.id", $input["productsGrids"])->get();
+                if($productsGrids->count() !== 2) $fail("$attribute, wrong products grids values");
+                if(!$productsGrids->contains("type", "size")) $fail("$attribute, product grid size is missed");
+                if(!$productsGrids->contains("type", "carrier")) $fail("$attribute, product grid size is missed");
+            }
+        }
+    }
+
+    private function checkDefects($device, $input, $fail, $attribute) {
+        collect($input["defects"])->each(function ($defectId) use ($device, $fail, $attribute) {
+            if(!$device->defects()->where("id", $defectId)->exists()) $fail("$attribute, device hasn't defect with id: $defectId");
+        });
     }
 }
