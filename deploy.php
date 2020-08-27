d4ae51f1a7c65e49bad22f6488a539bd2313b225
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2020-08-27 14:11:36 +0300

    Added fix into the AppServiceProvider and deploy.php files

--- deploy.php
+++ deploy.php
@@ -33,7 +33,7 @@ host('development')
     ->multiplexing(true)
     ->addSshOption('UserKnownHostsFile', '~/.ssh/known_hosts')
     ->addSshOption('StrictHostKeyChecking', 'no')
-    ->set("composer_options", "install")
+    ->set("composer_options", "install --optimize-autoloader --no-dev")
     ->set('deploy_path', '/var/www/test19/data/www/test19.f5-cloud.top');
     
 // Tasks
@@ -41,6 +41,23 @@ host('development')
 /*task('build', function () {
     run('cd {{release_path}} && build');
 });*/
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
