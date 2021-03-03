729f9d39db3654353dfbc37fc081f213930da3a4
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2021-03-03 12:20:04 +0200

    Added functionality for search customers

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -12,4 +12,36 @@ class CustomersController extends Controller
         $customers = Customer::paginate(10);
         return (new CustomersCollection($customers))->response()->getData(true);
     }
+
+    public function search(Request $request) {
+        if (empty($request->qs)) {
+            return null;
+        }
+
+        $query = trim($request->qs);
+        $separatorPos = stripos($query, ' ');
+        $customers = null;
+
+        if ($separatorPos > 0) {
+            $firstPart = substr($query, 0, $separatorPos);
+            $secondPart = substr($query, $separatorPos + 1);
+
+            $customers = Customer::where('first_name', 'LIKE', "%{$firstPart}%")
+                ->where('last_name', 'LIKE', "%{$secondPart}%")
+                ->orWhere('first_name', 'LIKE', "%{$secondPart}%")
+                ->where('last_name', 'LIKE', "%{$firstPart}%")
+                ->get();
+
+        } else {
+            if (filter_var($query, FILTER_VALIDATE_EMAIL)) {
+                $customers = Customer::where('email', 'LIKE', "%{$query}%")->get();
+            } else {
+                $customers = Customer::where('first_name', 'LIKE', "%{$query}%")
+                    ->orWhere('last_name', 'LIKE', "%{$query}%")
+                    ->get();
+            }
+        }
+
+        return $customers;
+    }
 }
