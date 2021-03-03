fd2e33f104ec0316e25d5003d2b30aecb551fdc4
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2021-03-03 12:31:17 +0200

    Added pagination into the customers search

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -8,12 +8,21 @@ use App\Http\Resources\Customers\CustomersCollection;
 
 class CustomersController extends Controller
 {
-    public function getCustomers() {
+    public $itemsPerPage;
+
+    public function __construct(Request $request)
+    {
+        $this->itemsPerPage = env('DASHBOARD_ITEMS_PER_PAGE');
+    }
+
+    public function getCustomers()
+    {
         $customers = Customer::paginate(10);
         return (new CustomersCollection($customers))->response()->getData(true);
     }
 
-    public function search(Request $request) {
+    public function search(Request $request)
+    {
         if (empty($request->qs)) {
             return null;
         }
@@ -30,15 +39,15 @@ class CustomersController extends Controller
                 ->where('last_name', 'LIKE', "%{$secondPart}%")
                 ->orWhere('first_name', 'LIKE', "%{$secondPart}%")
                 ->where('last_name', 'LIKE', "%{$firstPart}%")
-                ->get();
+                ->paginate($this->itemsPerPage);
 
         } else {
             if (filter_var($query, FILTER_VALIDATE_EMAIL)) {
-                $customers = Customer::where('email', 'LIKE', "%{$query}%")->get();
+                $customers = Customer::where('email', 'LIKE', "%{$query}%")->paginate($this->itemsPerPage);
             } else {
                 $customers = Customer::where('first_name', 'LIKE', "%{$query}%")
                     ->orWhere('last_name', 'LIKE', "%{$query}%")
-                    ->get();
+                    ->paginate($this->itemsPerPage);
             }
         }
 
