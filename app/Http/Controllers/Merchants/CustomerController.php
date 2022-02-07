b8f9fcbc4bc8d6d9696061fe532487816eb97e3b
Vladimir Zakharchenko <web.jungle.ua@gmail.com>
2022-02-07 17:33:28 +0200

    Added new devices search logic (for the WP search)

--- app/Http/Controllers/Merchants/CustomerController.php
+++ app/Http/Controllers/Merchants/CustomerController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Jobs\CustomerAutoRegistrationNotificationJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class CustomerController extends Controller
 {
